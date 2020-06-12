ce6d4723b27049e9924434b7920352a3eaa0cc9e
Manoj Rathinaswamy <manojkr2608@gmail.com>
2020-06-11 22:26:22 -0700

    Gives a list of peptides not covered

--- ScriptAssist.py
+++ ScriptAssist.py
@@ -8,10 +8,23 @@ limitArr=[]
 timePts=int(len(data[0,3:])/5)
 lastCol=3+timePts*2
 ttestColNo=4*timePts
+protLen=input("Give the length of the protein: ")
+nocArr=[]
+if int(data[0,1])!=1:
+    nocArr.append(F'1-{int(data[0,0])-1}')
+for i in range(1,num_rows):
+    lastEnd=int(data[i-1,1])
+    start=int(data[i,0])
+    if start>lastEnd+1:
+        nocArr.append(F'{lastEnd+1}-{start-1}')
+if int(data[num_rows-1,1])<int(protLen):
+    nocArr.append(F'{int(data[num_rows-1,1])+1}-{protLen}')
+d=","
+print(F'Peptides not covered in this data set: {d.join(nocArr)}')    
 varType=input("Is this a protection or exposure? (Type P for protection and E for exposure): ")
 while varType!='P' and varType!='E':
     varType=input("Invalid Entry. Type P or E: ")
-upper=input("Enter upper-limit (enter 1000 if no upper-limit): ")
+upper=input("Enter upper-limit: ")
 limitArr.append(upper)
 lower=input("Enter lower-limit: ")
 limitArr.append(lower)
@@ -25,7 +38,6 @@ if varType=='P':
                 if not string in pepArr:
                     pepArr.append(string)
             x-=1
-    d=","
     print(d.join(pepArr))
 if varType=='E':
     l=-int(limitArr[1])
@@ -38,13 +50,7 @@ if varType=='E':
                 if not string in pepArr:
                     pepArr.append(string)
             x-=1
-    d=","
     print(d.join(pepArr))
     
     
     
-
-    
-    
-    
-
